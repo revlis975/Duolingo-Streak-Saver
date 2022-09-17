b724f7acfdc4947a569d114dff2b1dcb3d3d9e30
Aryan Gupta <aryan.199301088@muj.manipal.edu>
2022-09-18 03:11:40 +0530

    Update duolingo.py
    Improved code quality

--- duolingo.py
+++ duolingo.py
@@ -3,6 +3,7 @@ import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 import ssl
 
 ctx = ssl.create_default_context()
@@ -15,137 +16,78 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--output=/dev/null")
 chrome_options.add_argument("--mute-audio")
 
-
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.duolingo.com")
-
-account = '//*[@id="root"]/div/div/span[1]/div/div[1]/div[2]/div[2]/a'
-username_input = '//*[@id="overlays"]/div[5]/div/div[2]/form/div[1]/div[1]/div[1]/label/div/input'
-password_input = '//*[@id="overlays"]/div[5]/div/div[2]/form/div[1]/div[1]/div[2]/label/div[1]/input'
-login_submit = '//*[@id="overlays"]/div[5]/div/div[2]/form/div[1]/button/span'
-
-WebDriverWait(driver,30).until(EC.presence_of_element_located((By.CSS_SELECTOR,"button[data-test='have-account']"))).click()
+wait = WebDriverWait(driver,30)
+wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"button[data-test='have-account']"))).click()
 
 # --------- TODO -------------
 
 #ENTER YOUR DUOLINGO EMAIL AND PASSOWRD FOR LOGIN
 
-
-driver.find_element_by_css_selector("input[data-test='email-input']").send_keys("enter your email")
-driver.find_element_by_css_selector("input[data-test='password-input']").send_keys("enter password")
-button = driver.find_element_by_css_selector("button[data-test='register-button']")
+driver.find_element_by_css_selector("input[data-test='email-input']").send_keys("Enter Duolingo email")
+driver.find_element_by_css_selector("input[data-test='password-input']").send_keys("Enter password")
+button = driver.find_element_by_css_selector("button[type='submit']")
 driver.execute_script("arguments[0].click();", button)
+time.sleep(20)
 
-time.sleep(5)
-
-try:
-	WebDriverWait(driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div[5]/div/div/div[1]/div/div[2]/div[1]/div/div[5]/div/div/div/div[1]/div/div[1]/div[1]'))).click()
-	time.sleep(3)
-	WebDriverWait(driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div[5]/div/div/div[1]/div/div[2]/div[1]/div/div[5]/div/div[2]/div/div[1]/div[4]/a'))).click()
-except:
-	try:
-		WebDriverWait(driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@id="overlays"]/div[4]/div/div[2]/button/span'))).click()
-		time.sleep(3)
-		WebDriverWait(driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div[5]/div/div/div/div[1]/div/div[2]/div[1]/div/div[5]/div/div[2]/div/div[1]/div[4]/a'))).click()
-	except:
-		WebDriverWait(driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@id="overlays"]/div[4]/div/div/div/div[2]/button'))).click()
-		time.sleep(3)
-		WebDriverWait(driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div[5]/div/div/div/div[1]/div/div[2]/div[1]/div/div[5]/div/div[2]/div/div[1]/div[4]/a'))).click()
-WebDriverWait(driver,30).until(EC.presence_of_element_located((By.CSS_SELECTOR,"button[data-test='player-toggle-keyboard']"))).click()
+driver.execute_script("arguments[0].click();", driver.find_element_by_xpath('//*[text()="Flirting"]'))
+time.sleep(10)
+driver.execute_script("arguments[0].click();", driver.find_element_by_css_selector("a[data-test='start-button']"))
+wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"button[data-test='player-toggle-keyboard']"))).click()
 
 gWords = []
 size = []
 
 for i in range(10):	
 
-	time.sleep(0.5)
-	
+	time.sleep(0.5)	
 	size.append(len(driver.find_elements_by_xpath('//*[@id="root"]/div/div/div/div/div[2]/div/div/div/div/div[2]/div[1]/div/span/div')))
-	
 	nsize = size[i]  
-
 	gWords = []
 
 	for j in range(1,nsize+1):
-		gWords.append(WebDriverWait(driver,30).until(EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div/div/div/div[2]/div/div/div/div/div[2]/div[1]/div/span/div['+str(j)+']'))).text)
+		gWords.append(wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div/div/div/div[2]/div/div/div/div/div[2]/div[1]/div/span/div['+str(j)+']'))).text)
 
 	s = " "
-
 	s = s.join(gWords)
-	 
-	ele = WebDriverWait(driver,30).until(EC.presence_of_element_located((By.CSS_SELECTOR,"textarea[data-test='challenge-translate-input']")))
+	
+	ele = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"textarea[data-test='challenge-translate-input']")))
 
-	if(s.startswith('Darf ich dich zum')):
-		ele.send_keys("May I invite you to dinner")
+	translations = {
+	  "Darf ich dich zum abendessen einladen": "May I invite you to dinner",
+	  "Ich finde dich nett": "I think you are nice",
+	  "Der Kaffee geht auf mich": "The coffee is on me",
+	  "Deine Augen sind wie Sterne.": "Your eyes are like stars",
+	  "Ich finde dich süß": "I think you're cute",
+	  "Willst du tanzen": "Do you want to dance",
+	  "Darf ich dich küssen": "May I kiss you",
+	  "Ich möchte dich besser kennen lernen": "I would like to get to know you better",
+	  "Du siehst aus wie meine nachste freundin": "You look like my next girlfriend",
+	  "Ich bin neu hier, und du?": "I am new here, and you",
+	  "Du bist schlau": "You are smart",
+	  "Du bist witzig": "You are funny",
+	  "Kann ich dir ein Getränk bestellen": "Can I order you a drink",
+	  "Ich hab mich in dich verliebt": "I have fallen in love with you",
+	  "Du kannst gut tanzen!": "You can dance well",
+	  "Ich liebe dich": "I love you",
+	  "Ich mag dich": "I like you",
+	  "Kann ich dich anrufen": "Can I call you",
+	  "Willst du mit mir ausgehen": "Do you want to go out with me"
+	}
 
-	if(s.startswith('Ich finde dich nett')):
-		ele.send_keys("I think you are nice")
+	if s in translations.keys():
+		ele.send_keys(translations[s])
 
-	if(s.startswith('Der Kaffee')):
-		ele.send_keys("The coffee is on me")
-
-	if(s.startswith('Deine Augen sind')):
-		ele.send_keys("Your eyes are like stars")
-
-	if(s.startswith('Ich finde dich süß')):
-		ele.send_keys("I think you're cute")
-
-	if(s.startswith('Willst du tanzen')):
-		ele.send_keys("Do you want to dance")
-
-	if(s.startswith('Darf ich dich küssen')):
-		ele.send_keys("May I kiss you")
-
-	if(s.startswith('Ich möchte dich besser')):
-		ele.send_keys("I would like to get to know you better")
-
-	if(s.startswith('Du siehst aus wie meine')):
-		ele.send_keys("You look like my next girlfriend")
-
-	if(s.startswith('Ich bin neu')):
-		ele.send_keys("I am new here, and you")
-
-	if(s.startswith('Du bist schlau')):
-		ele.send_keys("You are smart")
-
-	if(s.startswith('Du bist witzig')):
-		ele.send_keys("You are funny")
-
-	if(s.startswith('Kann ich dir ein')):
-		ele.send_keys("Can I order you a drink")
-
-	if(s.startswith('Ich hab mich in dich verliebt')):
-		ele.send_keys("I have fallen in love with you")
-
-	if(s.startswith('Du kannst gut tanzen')):
-		ele.send_keys("You can dance well")
-
-	if(s.startswith('Ich liebe dich')):
-		ele.send_keys("I love you")
-
-	if(s.startswith('Ich mag dich')):
-		ele.send_keys("I like you")
-
-	if(s.startswith('Kann ich dich anrufen')):
-		ele.send_keys("Can I call you")
-
-	if(s.startswith('Willst du mit mir ausgehen')):
-		ele.send_keys("Do you want to go out with me")
-
-	WebDriverWait(driver,30).until(EC.presence_of_element_located((By.CSS_SELECTOR,"button[data-test='player-next']"))).click()
-
-	WebDriverWait(driver,30).until(EC.presence_of_element_located((By.CSS_SELECTOR,"button[data-test='player-next']"))).click()
-
+	wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"button[data-test='player-next']"))).click()
+	wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"button[data-test='player-next']"))).click()
 	time.sleep(0.5)
 	if(i==4):
-
-		WebDriverWait(driver,30).until(EC.presence_of_element_located((By.CSS_SELECTOR,"button[data-test='player-next']"))).click()
-
+		wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"button[data-test='player-next']"))).click()
 	i = i+1
 
-print("Congratulations on continuing your streak!!")
+print("Congratulations on continuing your streak!")
 time.sleep(4)
 driver.find_element_by_css_selector("button[data-test='player-next']").click()
 time.sleep(7)
 driver.close()
-
