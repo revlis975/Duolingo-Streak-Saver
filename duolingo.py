bd295ae8ed38c1d45fe0377f26e7e00de18a1d12
Aryan <aryanjigupta@gmail.com>
2022-10-01 16:56:02 +0530

    Improved code quality

--- duolingo.py
+++ duolingo.py
@@ -66,62 +66,30 @@ for i in range(10):
 	 
 	ele = WebDriverWait(driver,30).until(EC.presence_of_element_located((By.CSS_SELECTOR,"textarea[data-test='challenge-translate-input']")))
 
-	if(s.startswith('Darf ich dich zum')):
-		ele.send_keys("May I invite you to dinner")
+	translations = {
+		"Darf ich dich zum abendessen einladen": "May I invite you to dinner",
+		"Ich finde dich nett": "I think you are nice",
+		"Der Kaffee geht auf mich": "The coffee is on me",
+		"Deine Augen sind wie Sterne.": "Your eyes are like stars",
+		"Ich finde dich süß": "I think you're cute",
+		"Willst du tanzen": "Do you want to dance",
+		"Darf ich dich küssen": "May I kiss you",
+		"Ich möchte dich besser kennen lernen": "I would like to get to know you better",
+		"Du siehst aus wie meine nachste freundin": "You look like my next girlfriend",
+		"Ich bin neu hier, und du?": "I am new here, and you",
+		"Du bist schlau": "You are smart",
+		"Du bist witzig": "You are funny",
+		"Kann ich dir ein Getränk bestellen": "Can I order you a drink",
+		"Ich hab mich in dich verliebt": "I have fallen in love with you",
+		"Du kannst gut tanzen!": "You can dance well",
+		"Ich liebe dich": "I love you",
+		"Ich mag dich": "I like you",
+		"Kann ich dich anrufen": "Can I call you",
+		"Willst du mit mir ausgehen": "Do you want to go out with me"
+	}
 
-	if(s.startswith('Ich finde dich nett')):
-		ele.send_keys("I think you are nice")
-
-	if(s.startswith('Der Kaffee')):
-		ele.send_keys("The coffee is on me")
-
-	if(s.startswith('Deine Augen sind')):
-		ele.send_keys("Your eyes are like stars")
-
-	if(s.startswith('Ich finde dich süß')):
-		ele.send_keys("I think you're cute")
-
-	if(s.startswith('Willst du tanzen')):
-		ele.send_keys("Do you want to dance")
-
-	if(s.startswith('Darf ich dich küssen')):
-		ele.send_keys("May I kiss you")
-
-	if(s.startswith('Ich möchte dich besser')):
-		ele.send_keys("I would like to get to know you better")
-
-	if(s.startswith('Du siehst aus wie meine')):
-		ele.send_keys("You look like my next girlfriend")
-
-	if(s.startswith('Ich bin neu')):
-		ele.send_keys("I am new here, and you")
-
-	if(s.startswith('Du bist schlau')):
-		ele.send_keys("You are smart")
-
-	if(s.startswith('Du bist witzig')):
-		ele.send_keys("You are funny")
-
-	if(s.startswith('Kann ich dir ein')):
-		ele.send_keys("Can I order you a drink")
-
-	if(s.startswith('Ich hab mich in dich verliebt')):
-		ele.send_keys("I have fallen in love with you")
-
-	if(s.startswith('Du kannst gut tanzen')):
-		ele.send_keys("You can dance well")
-
-	if(s.startswith('Ich liebe dich')):
-		ele.send_keys("I love you")
-
-	if(s.startswith('Ich mag dich')):
-		ele.send_keys("I like you")
-
-	if(s.startswith('Kann ich dich anrufen')):
-		ele.send_keys("Can I call you")
-
-	if(s.startswith('Willst du mit mir ausgehen')):
-		ele.send_keys("Do you want to go out with me")
+	if s in translations.keys():
+		ele.send_keys(translations[s])
 
 	WebDriverWait(driver,30).until(EC.presence_of_element_located((By.CSS_SELECTOR,"button[data-test='player-next']"))).click()
 
